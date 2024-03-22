2ef322d149e9c0956780d54f12b9297fc4d5714f
chenjianwu <chenjianwu@chucp.mobi>
2024-03-22 14:47:55 +0800

    dialogFragment代码优化

--- src/main/kotlin/other/fragmentDialog/app_package/mvvmDialogFragmentKt.kt
+++ src/main/kotlin/other/fragmentDialog/app_package/mvvmDialogFragmentKt.kt
@@ -3,33 +3,48 @@ package other.fragmentDialog.app_package
 
 fun mvvmDialogFragmentKt(
     applicationPackage: String?,
-    activityClass: String,
+    dialogName: String,
     layoutName: String,
     packageName: String,
     needVM: Boolean
 ): String {
     val vmClassName = if (needVM) {
-        "${activityClass}VM"
+        "${dialogName}VM"
     } else {
         "BaseViewModel"
     }
+
+    val dialogFullClassName = "${dialogName}Dialog"
+    val bindingFullClassName = "Dialog${dialogName}Binding"
     return """
 package ${packageName}
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.FragmentManager
 import ${applicationPackage}.R
-import ${applicationPackage}.databinding.Dialog${activityClass}Binding
+import ${applicationPackage}.databinding.Dialog${dialogName}Binding
 import com.duiud.bobo.framework.viewmodel.BaseViewModel
 import com.duiud.bobo.framework.fragment.ViewModelDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ${activityClass}Dialog : ViewModelDialog<${vmClassName}, Dialog${activityClass}Binding>() {
+class ${dialogFullClassName} : ViewModelDialog<${vmClassName}, ${bindingFullClassName}>() {
+    companion object {
+        private const val TAG = "${dialogFullClassName}"
+        fun show(fragmentManager: FragmentManager): ${dialogFullClassName} {
+            val args = Bundle()
+            val fragment = ${dialogFullClassName}()
+            fragment.arguments = args
+            fragment.show(fragmentManager, TAG)
+            return fragment
+        }
+    }
     override fun getLayoutId() = R.layout.${layoutName}
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //todo Write business here 
     }
 }
 """
